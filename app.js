a7397b5f146a8c18828c66a3d66963d757155cca
Ahsan Tariq <61455261+ahsantariq792@users.noreply.github.com>
2021-07-09 18:49:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function load() {
+    a = document.getElementsByClassName("loadmore")
+    a[0].style.display = "block"
+    console.log(a)
+    b = document.getElementById("loadbtn")
+    b.style.display = "none"
+
+
+}
